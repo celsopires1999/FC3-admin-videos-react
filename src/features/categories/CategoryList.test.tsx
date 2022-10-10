63afaff7c597be848b189509ad8de2925336ee14
celsopires1999 <celso.pires1999@gmail.com>
2022-10-10 02:20:27 +0000

    test: Testes-category filter items

--- src/features/categories/CategoryList.test.tsx
+++ src/features/categories/CategoryList.test.tsx
@@ -74,4 +74,22 @@ describe("CategoryList", () => {
       expect(name).toBeInTheDocument();
     });
   });
+
+  it("should handle filter change", async () => {
+    renderWithProviders(<CategoryList />);
+
+    await waitFor(() => {
+      const name = screen.getByText("Docker");
+      expect(name).toBeInTheDocument();
+    });
+
+    const input = screen.getByPlaceholderText("Search…");
+
+    fireEvent.change(input, { target: { value: "Chocolate" } });
+
+    await waitFor(() => {
+      const loading = screen.getByRole("progressbar");
+      expect(loading).toBeInTheDocument();
+    });
+  });
 });
