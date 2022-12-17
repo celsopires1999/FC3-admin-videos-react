f0d03bdc78a2a14b43668dc329a9f9312d6b50bc
celsopires1999 <celso.pires1999@gmail.com>
2022-12-17 23:16:38 +0000

    fix: Ajustando o layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Typography } from "@mui/material";
+import { CssBaseline, Typography } from "@mui/material";
 import { Box, ThemeProvider } from "@mui/system";
 import { SnackbarProvider } from "notistack";
 import { Route, Routes } from "react-router-dom";
@@ -21,61 +21,47 @@ import { VideoList } from "./features/videos/VideoList";
 function App() {
   return (
     <ThemeProvider theme={appTheme}>
+      <CssBaseline />
       <SnackbarProvider
         autoHideDuration={2000}
         maxSnack={3}
         anchorOrigin={{ vertical: "top", horizontal: "right" }}
       >
-        <Box
-          component="main"
-          sx={{
-            height: "100vh",
-            minHeight: 650,
-            backgroundColor: (theme) => theme.palette.grey[900],
-          }}
-        >
-          <Header />
-          <Layout>
-            <Routes>
-              <Route path="/" element={<CategoryList />} />
-              {/* Category */}
-              <Route path="/categories" element={<CategoryList />} />
-              <Route path="/categories/create" element={<CategoryCreate />} />
-              <Route path="/categories/edit/:id" element={<CategoryEdit />} />
+        <Header />
+        <Layout>
+          <Routes>
+            <Route path="/" element={<CategoryList />} />
+            {/* Category */}
+            <Route path="/categories" element={<CategoryList />} />
+            <Route path="/categories/create" element={<CategoryCreate />} />
+            <Route path="/categories/edit/:id" element={<CategoryEdit />} />
 
-              {/* Cast Members */}
-              <Route path="/cast-members" element={<CastMemberList />} />
-              <Route
-                path="/cast-members/create"
-                element={<CastMemberCreate />}
-              />
-              <Route
-                path="/cast-members/edit/:id"
-                element={<CastMemberEdit />}
-              />
+            {/* Cast Members */}
+            <Route path="/cast-members" element={<CastMemberList />} />
+            <Route path="/cast-members/create" element={<CastMemberCreate />} />
+            <Route path="/cast-members/edit/:id" element={<CastMemberEdit />} />
 
-              {/* Genres */}
-              <Route path="/genres" element={<GenreList />} />
-              <Route path="/genres/create" element={<GenreCreate />} />
-              <Route path="/genres/edit/:id" element={<GenreEdit />} />
+            {/* Genres */}
+            <Route path="/genres" element={<GenreList />} />
+            <Route path="/genres/create" element={<GenreCreate />} />
+            <Route path="/genres/edit/:id" element={<GenreEdit />} />
 
-              {/* Videos */}
-              <Route path="/videos" element={<VideoList />} />
-              <Route path="/videos/create" element={<VideoCreate />} />
-              <Route path="/videos/edit/:id" element={<VideoEdit />} />
+            {/* Videos */}
+            <Route path="/videos" element={<VideoList />} />
+            <Route path="/videos/create" element={<VideoCreate />} />
+            <Route path="/videos/edit/:id" element={<VideoEdit />} />
 
-              <Route
-                path="*"
-                element={
-                  <Box sx={{ color: "white" }}>
-                    <Typography variant="h1">404</Typography>
-                    <Typography variant="h2">Page not found</Typography>
-                  </Box>
-                }
-              />
-            </Routes>
-          </Layout>
-        </Box>
+            <Route
+              path="*"
+              element={
+                <Box sx={{ color: "white" }}>
+                  <Typography variant="h1">404</Typography>
+                  <Typography variant="h2">Page not found</Typography>
+                </Box>
+              }
+            />
+          </Routes>
+        </Layout>
       </SnackbarProvider>
     </ThemeProvider>
   );
